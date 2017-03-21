dbca3362fc1800b930625193242fe8eba95374c9
allen <allengun2000@gmail.com>
2017-03-21 23:29:11 +0800

    106/3/21 rostopic var can scran

--- vision/interface/interface.cpp
+++ vision/interface/interface.cpp
@@ -8,9 +8,71 @@ using namespace std;
 
 void InterfaceProc::ParameterButtonCall (const vision::parameterbutton msg)
 {
-	std::cout<<msg.button<<std::endl;
+    buttonmsg=msg.button;
+    std::cout<<buttonmsg<<std::endl;
 }
+void InterfaceProc::colorcall(const vision::color msg){
+     ColorModeMsg=msg.ColorMode;
+     switch(ColorModeMsg){
+     case 0:
+     for(int i=0;i<6;i++)
+         BallHSVBoxMsg[i]=msg.BallHSVBox[i];
+         break;
+     case 1:
+     for(int i=0;i<6;i++)
+         GreenHSVBoxMsg[i]=msg.GreenHSVBox[i];
+         break;
+     case 2:
+     for(int i=0;i<6;i++)
+         BlueHSVBoxMsg[i]=msg.BlueHSVBox[i];
+         break;
+     case 3:
+     for(int i=0;i<6;i++)
+         YellowHSVBoxMsg[i]=msg.YellowHSVBox[i];
+         break;
+     case 4:
+     for(int i=0;i<6;i++)
+         WhiteHSVBoxMsg[i]=msg.WhiteHSVBox[i];
+         break;
+     }
+    std::cout<<BallHSVBoxMsg[3]<<std::endl;
 
+}
+void InterfaceProc::centercall(const vision::center msg){
+        CenterXMsg=msg.CenterX;
+        CenterYMsg=msg.CenterY;
+        InnerMsg=msg.Inner;
+        OuterMsg=msg.Outer;
+        FrontMsg=msg.Front;
+        Camera_HighMsg=msg.Camera_High;
+}
+void InterfaceProc::whitecall(const vision::white msg){
+    WhiteGrayMsg=msg.Gray;
+    WhiteAngleMsg=msg.Angle;
+}
+void InterfaceProc::cameracall(const vision::camera msg){
+fpsMsg=msg.fps;
+}
+void InterfaceProc::blackcall(const vision::black msg){
+    BlackGrayMsg=msg.Gray;
+    BlackAngleMsg=msg.Angle;
+}
+void InterfaceProc::colorbuttoncall(const vision::colorbutton msg){
+ colorbottonMsg=msg.button;
+}
+void InterfaceProc::scancall(const vision::scan msg){
+Angle_Near_GapMsg=msg.Angle_Near_Gap;
+Magn_Near_GapMsg=msg.Magn_Near_Gap;
+Magn_Near_StartMsg=msg.Magn_Near_Start;
+Magn_Middle_StartMsg=msg.Magn_Middle_Start;
+Magn_Far_StartMsg=msg.Magn_Far_Start;
+Magn_Far_EndMsg=msg.Magn_Far_End;
+Dont_Search_Angle_1Msg=msg.Dont_Search_Angle_1;
+Dont_Search_Angle_2Msg=msg.Dont_Search_Angle_2;
+Dont_Search_Angle_3Msg=msg.Dont_Search_Angle_3;
+Angle_range_1Msg=msg.Angle_range_1;
+Angle_range_2_3Msg=msg.Angle_range_2_3;
+}
 InterfaceProc::InterfaceProc()
 	:it_(nh)
 {
@@ -19,7 +81,13 @@ InterfaceProc::InterfaceProc()
 	image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, &InterfaceProc::imageCb, this);
 	image_pub_threshold_ = it_.advertise("/interface/image_raw/threshold", 1);
 	s1 = nh.subscribe("/interface/parameterbutton", 1000, &InterfaceProc::ParameterButtonCall, this);
-
+    s2 = nh.subscribe("interface/color", 1000, &InterfaceProc::colorcall,this);
+    s3 = nh.subscribe("interface/center", 1000, &InterfaceProc::centercall,this);
+    s4 = nh.subscribe("interface/white", 1000, &InterfaceProc::whitecall,this);
+    s5 = nh.subscribe("interface/camera", 1000, &InterfaceProc::cameracall,this);
+    s6 = nh.subscribe("interface/black", 1000, &InterfaceProc::blackcall,this);
+    s7 = nh.subscribe("interface/colorbutton", 1000, &InterfaceProc::colorbuttoncall,this);
+    s8 = nh.subscribe("interface/scan", 1000, &InterfaceProc::scancall,this);
 	cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_AUTOSIZE);
 	frame = new cv::Mat(cv::Size(FRAME_COLS, FRAME_ROWS), CV_8UC3);
 	gray = new cv::Mat(cv::Size(FRAME_COLS, FRAME_ROWS), CV_8UC3);
--- vision/interface/interface.hpp
+++ vision/interface/interface.hpp
@@ -8,7 +8,15 @@
 #include <iostream>
 #include <fstream>
 #include <vision/parameterbutton.h>
-
+#include "vision/center.h"
+#include"vision/white.h"
+#include"vision/camera.h"
+#include"vision/black.h"
+#include"vision/colorbutton.h"
+#include"vision/scan.h"
+#include "vision/color.h"
+#include <stdio.h>
+#include <stdlib.h>
 class InterfaceProc
 {
 private:
@@ -16,7 +24,14 @@ private:
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
 	image_transport::Publisher image_pub_threshold_;
-	ros::Subscriber s1;
+    ros::Subscriber s1;
+    ros::Subscriber s2;
+    ros::Subscriber s3;
+    ros::Subscriber s4;
+    ros::Subscriber s5;
+    ros::Subscriber s6;
+    ros::Subscriber s7;
+    ros::Subscriber s8;
 
 	cv::Mat *frame;
 	cv::Mat *gray;
@@ -24,8 +39,44 @@ private:
 public:
   InterfaceProc();
   ~InterfaceProc();
-
+    int buttonmsg;
+    int CenterXMsg;
+    int CenterYMsg;
+    int InnerMsg;
+    int OuterMsg;
+    int FrontMsg;
+    int Camera_HighMsg;
+    int colorbottonMsg;
+    int Angle_Near_GapMsg;
+    int Magn_Near_GapMsg;
+    int Magn_Near_StartMsg;
+    int Magn_Middle_StartMsg;
+    int Magn_Far_StartMsg;
+    int Magn_Far_EndMsg;
+    int Dont_Search_Angle_1Msg;
+    int Dont_Search_Angle_2Msg;
+    int Dont_Search_Angle_3Msg;
+    int Angle_range_1Msg;
+    int Angle_range_2_3Msg;
+    int BlackGrayMsg;
+    int BlackAngleMsg;
+    int WhiteGrayMsg;
+    int WhiteAngleMsg;
+    int fpsMsg;
+    int BallHSVBoxMsg[6];
+    int GreenHSVBoxMsg[6];
+    int BlueHSVBoxMsg[6];
+    int YellowHSVBoxMsg[6];
+    int WhiteHSVBoxMsg[6];
+    int ColorModeMsg;
 	void imageCb(const sensor_msgs::ImageConstPtr&);
 	void ParameterButtonCall(const vision::parameterbutton);
+    void colorcall(const vision::color);
+    void centercall(const vision::center);
+    void whitecall(const vision::white);
+    void cameracall(const vision::camera);
+    void blackcall(const vision::black);
+    void colorbuttoncall(const vision::colorbutton);
+    void scancall(const vision::scan);
 	cv::Mat Gray(const cv::Mat iframe);
 };
