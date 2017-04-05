a7e126c5a962e93959860ae93374899c673d1824
Eithwa <mich841012@gmail.com>
2017-04-05 14:31:05 +0800

    scan完成

--- vision/interface/interface.hpp
+++ vision/interface/interface.hpp
@@ -1,3 +1,7 @@
+#include <cstdio>
+#include <unistd.h>
+#include <opencv2/opencv.hpp>
+#include <math.h>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -17,12 +21,13 @@
 #include "vision/color.h"
 #include <stdio.h>
 #include <stdlib.h>
+using namespace cv;
 class InterfaceProc
 {
 private:
-  ros::NodeHandle nh;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
 	image_transport::Publisher image_pub_threshold_;
     ros::Subscriber s1;
     ros::Subscriber s2;
@@ -34,75 +39,88 @@ private:
     ros::Subscriber s8;
 
 	int StartTime,EndTime;
-    int core_num;
-    int fps_num[100];
-    int fps_avg;
+	int core_num;
+	int fps_num[100];
+	int fps_avg;
 
 	cv::Mat *frame;
     cv::Mat *ColorModels;
-	cv::Mat *centermats;
- int hmax,hmin,smax,smin,vmax,vmin;
+    cv::Mat *CenterModels;
+    cv::Mat *outputframe;
+	int hmax,hmin,smax,smin,vmax,vmin;
+
 public:
-  InterfaceProc();
-  ~InterfaceProc();
-    int buttonmsg;
-    int CenterXMsg;
-    int CenterYMsg;
-    int InnerMsg;
-    int OuterMsg;
-    int FrontMsg;
-    int Camera_HighMsg;
-    int colorbottonMsg;
-    int Angle_Near_GapMsg;
-    int Magn_Near_GapMsg;
-    int Magn_Near_StartMsg;
-    int Magn_Middle_StartMsg;
-    int Magn_Far_StartMsg;
-    int Magn_Far_EndMsg;
-    int Dont_Search_Angle_1Msg;
-    int Dont_Search_Angle_2Msg;
-    int Dont_Search_Angle_3Msg;
-    int Angle_range_1Msg;
-    int Angle_range_2_3Msg;
-    int BlackGrayMsg;
-    int BlackAngleMsg;
-    int WhiteGrayMsg;
-    int WhiteAngleMsg;
-    int fpsMsg;
-    int BallHSVBoxMsg[6];
-    int GreenHSVBoxMsg[6];
-    int BlueHSVBoxMsg[6];
-    int YellowHSVBoxMsg[6];
-    int WhiteHSVBoxMsg[6];
-    int ColorModeMsg;
+	InterfaceProc();
+	~InterfaceProc();
+	int buttonmsg;
+	int CenterXMsg;
+	int CenterYMsg;
+	int robotCenterX;
+	int robotCenterY;
+	int InnerMsg;
+	int OuterMsg;
+	int FrontMsg;
+	int camera_focal;
+	int Camera_HighMsg;
+	int colorbottonMsg;
+///////////scan/////////////
+	int Angle_Near_GapMsg;
+	int Magn_Near_GapMsg;
+	int Magn_Near_StartMsg;
+	int Magn_Middle_StartMsg;
+	int Magn_Far_StartMsg;
+	int Magn_Far_EndMsg;
+	int Dont_Search_Angle_1Msg;
+	int Dont_Search_Angle_2Msg;
+	int Dont_Search_Angle_3Msg;
+	int Angle_range_1Msg;
+	int Angle_range_2_3Msg;
+////////////////////////////
+	int BlackGrayMsg;
+	int BlackAngleMsg;
+	int WhiteGrayMsg;
+	int WhiteAngleMsg;
+	int fpsMsg;
+	int BallHSVBoxMsg[6];
+	int GreenHSVBoxMsg[6];
+	int BlueHSVBoxMsg[6];
+	int YellowHSVBoxMsg[6];
+	int WhiteHSVBoxMsg[6];
+	int ColorModeMsg;
 
 	void imageCb(const sensor_msgs::ImageConstPtr&);
 	void ParameterButtonCall(const vision::parameterbutton);
-    void colorcall(const vision::color);
-    void centercall(const vision::center);
-    void whitecall(const vision::white);
-    void cameracall(const vision::camera);
-    void blackcall(const vision::black);
-    void colorbuttoncall(const vision::colorbutton);
-    void scancall(const vision::scan);
+	void colorcall(const vision::color);
+	void centercall(const vision::center);
+	void whitecall(const vision::white);
+	void cameracall(const vision::camera);
+	void blackcall(const vision::black);
+	void colorbuttoncall(const vision::colorbutton);
+	void scancall(const vision::scan);
 	void Parameter_setting(const int x) ;
-    cv::Mat ColorModel(const cv::Mat iframe);
-
 	int mosue_x,mosue_y;
-    int distance_space[100];
-    int distance_pixel[100];
-    int HSV_init[6];
+	int distance_space[100];
+	int distance_pixel[100];
+	int HSV_init[6];
 
-    std::vector<int>scan_para;
-    std::vector<int>scan_near;
-    std::vector<int>scan_middle;
-    std::vector<int>scan_far;
+	std::vector<int>scan_para;
+	std::vector<int>scan_near;
+	std::vector<int>scan_middle;
+	std::vector<int>scan_far;
 
-    std::vector<int>dis_space;
-    std::vector<int>dis_pixel;
+	std::vector<int>dis_space;
+	std::vector<int>dis_pixel;
 
-    std::vector<int> HSV_red;
-    std::vector<int> HSV_green;
-    std::vector<int> HSV_blue;
-    std::vector<int> HSV_yellow;
+	std::vector<int> HSV_red;
+	std::vector<int> HSV_green;
+	std::vector<int> HSV_blue;
+	std::vector<int> HSV_yellow;
+
+	double camera_f(int Omni_pixel);
+	double Omni_distance(int object_x , int object_y);
+
+	cv::Mat ColorModel(const cv::Mat iframe);
+	cv::Mat CenterModel(const cv::Mat iframe);
+	cv::Mat ScanModel(const cv::Mat iframe);
+
 };
